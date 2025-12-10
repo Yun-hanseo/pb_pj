61f74ae0672c7074c796033f9aed4907330a4548
Yun-hanseo <gogodoom59@gmail.com>
2025-12-11 02:51:00 +0900

    메인화면에서 전환효과 fade추가

--- src/App.vue
+++ src/App.vue
@@ -30,17 +30,54 @@ html, body {
   background-color: #1a1a1d;
   width: 100%;
   height: 100%;
+  overflow-x: hidden !important;
 }
 
 .app-wrapper {
+  position: relative;
   min-height: 100vh;
   width: 100%;
   background-color: #1a1a1d;
   padding-top: 70px;
+  overflow-x: hidden;
 }
 
 .no-padding {
   padding-top: 0 !important;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+
+
+@keyframes nf-slide-in {
+  from {
+    opacity: 0;
+    transform: translateX(40px); /* 오른쪽에서 등장 */
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+@keyframes nf-slide-out {
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(-40px); /* 왼쪽으로 나감 */
+  }
 }
 </style>
 
